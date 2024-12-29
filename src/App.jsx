fea48ae45aa3ab0e531be06d40bbd0719dd38871
Mykhailo Kalinichenko <99479355+fidelmigel@users.noreply.github.com>
2024-12-29 16:48:30 +0100

    31' 01"

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,43 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect, useState } from "react";
+import axios from "axios";
 
-function App() {
-  const [count, setCount] = useState(0)
+const App = () => {
+  const [articles, setArticles] = useState([]);
+
+  useEffect(() => {
+    // axios
+    //   .get("https://hn.algolia.com/api/v1/search?query=react")
+    //  .then((res) => {
+    //    setArticles(res.data.hits);
+    // попередній приклад працює, але на практиці використовують наступний спосіб
+
+    const getArticlesData = async () => {
+      try {
+        const res = await axios.get(
+          "https://hn.algolia.com/api/v1/search?query=react"
+        );
+        setArticles(res.data.hits);
+      } catch (error) {
+        console.error(error);
+      }
+    };
+    getArticlesData();
+  }, []);
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+    <div>
+      <h1>Articles</h1>
+      <ul>
+        {articles.map((item) => (
+          <li key={item.objectID}>
+            <a href={item.url} target="_blank">
+              {item.title}
+            </a>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
 
-export default App
+export default App;
