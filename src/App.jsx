77458fbd12c71bdacc056e9c34c924c8effdf414
Mykhailo Kalinichenko <99479355+fidelmigel@users.noreply.github.com>
2025-01-06 02:12:40 +0100

    add

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,48 @@
 import { useEffect, useState } from "react";
-import axios from "axios";
+//import axios from "axios";
+import { fetchArticles } from "./services/api";
+//import { FidgetSpinner } from "react-loader-spinner";
+import Loader from "./components/Loader";
 
 const App = () => {
   const [articles, setArticles] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [isError, setIsError] = useState(false);
 
   useEffect(() => {
     const getArticlesData = async () => {
       try {
-        const { data } = await axios.get(
-          "https://hn.algolia.com/api/v1/search?query=react"
-        );
-        setArticles(data.hits);
+        setIsLoading(true);
+        console.log("Loading started");
+        setIsError(false);
+        const { hits } = await fetchArticles("react");
+        setArticles(hits);
       } catch (error) {
+        setIsError(true);
         console.error(error);
+      } finally {
+        setIsLoading(false);
       }
     };
     getArticlesData();
   }, []);
 
   return (
-    <div>
-      <h1>Articles</h1>
+    <div className="main">
+      {" "}
+      <h1>Articles</h1>{" "}
       <ul>
+        {" "}
         {articles.map((item) => (
           <li key={item.objectID}>
-            <a href={item.url}>{item.title}</a>
+            {" "}
+            <a href={item.url}>{item.title}</a>{" "}
           </li>
-        ))}
-      </ul>
+        ))}{" "}
+      </ul>{" "}
+      {isLoading && <Loader />}
+      {isError && <h2>Something went wrong! Try again...</h2>}{" "}
     </div>
   );
 };
-
 export default App;
--- src/services/api.js
+++ src/services/api.js
@@ -1,13 +1,8 @@
 import axios from "axios";
 
 export const fetchArticles = async (query) => {
-  try {
-    const { data } = await axios.get(
-      `https://hn.algolia.com/api/v1/search?query=${query}`
-    );
-    return data.hits;
-  } catch (error) {
-    console.error(error);
-    return [];
-  }
+  const { data } = await axios.get(
+    `https://hn.algolia.com/api/v1/search?query=${query}`
+  );
+  return data;
 };
