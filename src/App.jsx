fd86307a44fa43da12135b91e32b55c779a567f3
Mykhailo Kalinichenko <99479355+fidelmigel@users.noreply.github.com>
2025-01-07 15:09:19 +0100

    UseMemoExample

--- src/App.jsx
+++ src/App.jsx
@@ -1,57 +1,20 @@
-// App.js
-import { useEffect, useState } from "react";
-// import axios from "axios"; // Якщо не використовується, залиште закоментованим
-import { fetchArticles } from "./services/api"; // Переконайтесь, що функція fetchArticles існує
-import Loader from "./components/Loader"; // Перевірте шлях до Loader
-import Articles from "./components/Articles/Articles";
-import SearchBar from "./components/SearchBar/SearchBar";
-import { toast } from "react-hot-toast";
+//import UseContextExample from "./components/UseContextExample/UseContextExample";
+import UseMemoExample from "./components/UseMemoExample";
+//import UseRefRenderCountExample from "./components/UseRefForwardRefExample";
+//import UseRefStoreValueExample from "./components/UseRefStoreValueExample";
+//import UseRefForwardRefExample from "./components/UseRefForwardRefExample";
+
 const App = () => {
-  const [articles, setArticles] = useState([]);
-  const [isLoading, setIsLoading] = useState(false);
-  const [isError, setIsError] = useState(false);
-  const [page, setPage] = useState(0);
-  const [query, setQuery] = useState("");
-
-  useEffect(() => {
-    const getArticlesData = async () => {
-      try {
-        setIsLoading(true);
-        setIsError(false);
-        const { hits } = await fetchArticles(query, page);
-        setArticles((prev) => [...prev, ...hits]);
-      } catch (error) {
-        setIsError(true);
-        console.error(error);
-      } finally {
-        setIsLoading(false);
-      }
-    };
-    getArticlesData();
-  }, [page, query]);
-  const handleChangePage = () => {
-    toast.success(`Page changed to: ${page + 1}`);
-    setPage((prev) => prev + 1);
-  };
-
-  const handleChangeQuery = (newQuery) => {
-    if (query === newQuery) {
-      toast.error("Please change  query!");
-      return;
-    }
-
-    setQuery(newQuery);
-    setArticles([]);
-    setPage(0);
-  };
   return (
-    <div className="main">
-      <h1>Articles</h1>
-      <SearchBar onSearchChanged={handleChangeQuery} />
-      {articles.length > 0 && <Articles articles={articles} />}
-      <button onClick={handleChangePage}>Load more</button>
-      {isLoading && <Loader />}
-      {isError && <h2>Something went wrong! Try again...</h2>}
+    <div>
+      {/*<Header /> */}
+      <main>
+        <UseMemoExample />
+        {/*<UseRefRenderCountExample /> */}
+        {/*<UseRefStoreValueExample /> */}
+        {/*<UseRefForwardRefExample /> */}
+        {/* <UseContextExample /> */}
+      </main>
     </div>
   );
 };
